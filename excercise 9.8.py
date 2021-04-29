ec3304c480a74f979a0bff97545f42cd36470331
Nadya Indah Arifin <nandyaindah8@gmail.com>
2021-04-30 00:39:38 +0700

    Tugas 9

---
+++ excercise 9.8.py
@@ -0,0 +1,13 @@
+# Excercise 9.8
+
+# mengonversi list ke dalam array.array
+1i = [10,20,30,40,50]
+C = array.array('i')
+C.fromlist(1i)
+type(C)
+type 'array.array'
+for nilai in C:
+    print("%d"% nilai, end='')
+
+
+10, 20, 30, 40, 50
---
+++ excercise 9.9.py
@@ -0,0 +1,11 @@
+# Excercise 9.9
+
+A = array.array('i', [100,200,300,400,500])
+A
+array('i', [100,200,300,400,500])
+
+
+A[1] = -700 #mengubah elemen kedua
+A[4] = 800 #mengubah elemen kelima
+A
+array('i',[100,-700,300,400,800])
